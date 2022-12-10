65acb84ee727e7865b5d3ce9f395db59a2df08c5
Silktrader <silktrader@gmail.com>
2022-12-10 01:00:59 +0100

    Update CORS policy to account for "Authorization" headers

--- cmd/webapi/cors.go
+++ cmd/webapi/cors.go
@@ -5,14 +5,13 @@ import (
 	"net/http"
 )
 
-// applyCORSHandler applies a CORS policy to the router. CORS stands for Cross-Origin Resource Sharing: it's a security
-// feature present in web browsers that blocks JavaScript requests going across different domains if not specified in a
-// policy. This function sends the policy of this API server.
+// applyCORSHandler applies a CORS policy to the router.
 func applyCORSHandler(h http.Handler) http.Handler {
 	return handlers.CORS(
 		handlers.AllowedHeaders([]string{
-			"x-example-header",
+			"Content-Type", "Authorization",
 		}),
+		handlers.AllowCredentials(),
 		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"}),
 		handlers.AllowedOrigins([]string{"*"}),
 	)(h)
